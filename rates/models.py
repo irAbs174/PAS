1b51a4b269f4607fbf30b3d2c437815c2b78234e
48845 a <miror49735@sportrid.com>
2023-12-04 09:16:38 +0000

    update Rates model

--- rates/models.py
+++ rates/models.py
@@ -5,10 +5,15 @@ class Rates(models.Model):
     rate_author = models.CharField(max_length=40, verbose_name='ثبت کننده نرخ', null=True, blank=True)
     rate_value = models.FloatField(verbose_name='نرخ', blank=True, null=True)
     rate_unit = models.CharField(max_length=40, verbose_name='واحد نرخ', null=True, blank=True)
-    rate_date = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ و زمان نرخ گذاری', null=True, blank=True)
+    description = models.TextField(verbose_name='توضیحات', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد', blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='تاریخ بروزرسانی', blank=True, null=True)
 
     objects = models.Manager()
 
     class Meta:
         verbose_name = 'نرخ تولید'
         verbose_name_plural = 'نرخ های تولید'
+
+    def __str__(self):
+        return f"({self.rate_key})=>({self.rate_author}=>{self.rate_value})"
