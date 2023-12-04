c523e705ccfdca5ae26f41685fc1624d6157c632
48845 a <miror49735@sportrid.com>
2023-12-04 08:38:59 +0000

    Write tests for Rate model

---
+++ rates/tests.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from .models import Rates
+
+class RatesModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Rates.objects.create(
+            rate_key='TestKey',
+            rate_author='TestAuthor',
+            rate_value=100.5,
+            rate_unit='TestUnit'
+        )
+
+    def test_rates_model(self):
+        rate = Rates.objects.get(id=1)
+        self.assertEqual(rate.rate_key, 'TestKey')
+        self.assertEqual(rate.rate_author, 'TestAuthor')
+        self.assertAlmostEqual(rate.rate_value, 100.5)
+        self.assertEqual(rate.rate_unit, 'TestUnit')
+        self.assertIsNotNone(rate.rate_date)
