0451d1c177596aa3b54eb18e8c0d558c1658d73d
48845 a <miror49735@sportrid.com>
2023-12-04 09:14:50 +0000

    update materials admin.py

--- materials/admin.py
+++ materials/admin.py
@@ -2,9 +2,19 @@ from django.contrib import admin
 from .models import (Materials)
 
 class MaterialsAdmin(admin.ModelAdmin):
-    list_display = ('material_key', 'material_name', 'material_author', 'material_color', 'material_unit', 'material_date',)
-    list_filter = ('material_date', 'material_unit', 'material_author',)
-    search_fields = ('material_key', 'material_name', 'material_author', 'material_color', 'material_unit', 'material_date',)
+    list_display = ('material_key',
+        'material_name',
+        'material_author',
+        'material_color',
+        'material_unit',
+        'created_at',
+        'updated_at')
+    list_filter = ('created_at', 'material_unit', 'material_author',)
+    search_fields = ('material_key',
+        'material_name',
+        'material_author',
+        'material_color',
+        'material_unit',)
 
 
 admin.site.register(Materials, MaterialsAdmin)
