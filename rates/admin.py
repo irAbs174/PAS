41f6701b82e9b0782151a261516467f64926ece3
48845 a <miror49735@sportrid.com>
2023-12-04 09:16:20 +0000

    update rates app admin.py

--- rates/admin.py
+++ rates/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from .models import (Rates)
 
 class RatesAdmin(admin.ModelAdmin):
-    list_display = ('rate_key', 'rate_author', 'rate_value','rate_unit', 'rate_date')
-    list_filter = ('rate_author', 'rate_date', )
-    search_fields = ('rate_key', 'rate_author', 'rate_value','rate_unit', 'rate_date')
+    list_display = ('rate_key',
+        'rate_author',
+        'rate_value',
+        'rate_unit',
+        'created_at',
+        'updated_at')
+    list_filter = ('rate_author', 'created_at',)
+    search_fields = ('rate_key', 'rate_author', 'rate_value','rate_unit',)
 
 
 admin.site.register(Rates, RatesAdmin)
