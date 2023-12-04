073750f6b3e7496d5c07729811331efad4cfdf75
48845 a <miror49735@sportrid.com>
2023-12-04 08:17:44 +0000

    create tests for Materials models

---
+++ materials/tests.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from .models import Materials
+
+class MaterialsModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Materials.objects.create(
+            material_key='TestKey',
+            material_author='TestAuthor',
+            material_name='TestMaterial',
+            material_color='TestColor',
+            material_unit='TestUnit'
+        )
+
+    def test_materials_model(self):
+        material = Materials.objects.get(id=1)
+        self.assertEqual(material.material_key, 'TestKey')
+        self.assertEqual(material.material_author, 'TestAuthor')
+        self.assertEqual(material.material_name, 'TestMaterial')
+        self.assertEqual(material.material_color, 'TestColor')
+        self.assertEqual(material.material_unit, 'TestUnit')
+        self.assertIsNotNone(material.material_date)
