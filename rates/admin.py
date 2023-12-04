08cc8c94e74c4875e0d8de5ef407df3918f72f46
48845 a <miror49735@sportrid.com>
2023-12-04 08:37:42 +0000

    create admin.py for Rate model

--- rates/admin.py
+++ rates/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import (Rates)
 
-# Register your models here.
+class RatesAdmin(admin.ModelAdmin):
+    list_display = ('rate_key', 'rate_author', 'rate_value','rate_unit', 'rate_date')
+    list_filter = ('rate_author', 'rate_date', )
+    search_fields = ('rate_key', 'rate_author', 'rate_value','rate_unit', 'rate_date')
+
+
+admin.site.register(Rates, RatesAdmin)
