2ae5c3b85cec13ee867376fb76f3722c1de1320b
48845 a <miror49735@sportrid.com>
2023-12-25 13:14:04 +0000

    add S Alert Js

--- static/js/analys/new.js
+++ static/js/analys/new.js
@@ -1,7 +1,49 @@
+function createTable() {
+  var tableHTML = `
+      <table id="materials-table">
+          <thead>
+              <tr>
+                  <th>نام</th>
+                  <th>نماد</th>
+                  <th>توضیحات</th>
+                  <th>موجودی</th>
+                  <th>قیمت</th>
+                  <th>واحد</th>
+                  <th>عملیات</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr>
+                  <td><input type="text" class="نام ماده"></td>
+                  <td><input type="text" class="رنگ"></td>
+                  <td><input type="text" class="واحد"></td>
+                  <td><input type="number" class="نرخ" value="0"></td>
+                  <td><input type="number" class="تاریخ نرخ گذاری" value="0"></td>
+                  <td>
+                      <select class="unit">
+                          <option value="Unit1">متر</option>
+                          <option value="Unit2">عدد</option>
+                      </select>
+                  </td>
+                  <td><button onclick="deleteRow(this)">حذف ماده</button></td>
+              </tr>
+          </tbody>
+      </table>
+      <button onclick="addRow()">افزودن ماده اولیه جدید</button>
+      <button onclick="saveData()">ذخیره</button>
+  `;
+  document.getElementById('table-container').innerHTML = tableHTML;
+  document.getElementById('create-table-btn').style.display = "none"; // Hide the create button
+  document.getElementById('add-row-btn').style.display = "inline-block"; // Show add row button
+  document.getElementById('save-data-btn').style.display = "inline-block"; // Show save button
+  document.getElementById('add-row-btn').disabled = false; // Enable the add row button
+  document.getElementById('save-data-btn').disabled = false; // Enable the save button
+}
+
 function addRow() {
     const table = document.getElementById('materials-table').getElementsByTagName('tbody')[0];
     const newRow = table.insertRow(table.rows.length);
-    const columns = ['name', 'symbol', 'description', 'quantity', 'price', 'unit', 'action'];
+    const columns = ['نام', 'نماد', 'توضیحات', 'موجودی', 'قیمت', 'واحد', 'action'];
 
     columns.forEach((col, index) => {
       const cell = newRow.insertCell(index);
@@ -55,4 +97,42 @@ function addRow() {
 
     console.log(data);
     // Add logic to handle the data (e.g., sending it to a server, etc.)
+  }
+
+  function selectMaterial() {
+    // Make a request to the server to get the materials
+    fetch('materials_list')   // Make sure 'api/materials/' is the correct endpoint exposed by your Django server to get materials
+      .then(response => {
+        if (response.ok) {
+          return response.json(); // Convert the response to JSON
+        }
+        throw new Error('Network response was not ok.');
+      })
+      .then(materials => {
+        // Create the select box options based on the materials received
+        let options = materials.map(material =>
+          `<option value="${material.material_key}">${material.material_name}</option>`
+        ).join('');
+    
+        // Show the SweetAlert with dynamically created select box options
+        Swal.fire({
+          title: 'ماده اولیه را انتخاب کنید',
+          html: '<select id="swal-input" class="swal2-input">' + options + '</select>',
+          focusConfirm: false,
+          confirmButtonText: 'تائید',
+          preConfirm: () => {
+            const selectedKey = document.getElementById('swal-input').value;
+            return materials.find(material => material.material_key === selectedKey).material_name;
+          }
+        }).then((result) => {
+          if (result.isConfirmed) {
+            // Handle the result of the selection here
+            Swal.fire('شما انتخاب کردید: ' + result.value);
+          }
+        });
+      })
+      .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+        Swal.fire('خطا', 'درخواست به سرور با مشکل مواجه شد', 'error');
+      });
   }
