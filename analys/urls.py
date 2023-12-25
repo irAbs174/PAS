99a2abe18ed3dd324c6c5cfcb19f0e2d11490b8a
48845 a <miror49735@sportrid.com>
2023-12-25 13:13:26 +0000

    add urls

--- analys/urls.py
+++ analys/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import (new_analys)
+from .views import (new_analys, materials_list)
 
 urlpatterns = [
     path('new', new_analys, name="start_new_analys"),
+    path('materials_list', materials_list),
 ]
