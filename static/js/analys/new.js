013ba2e0a7aca0e59f1c99399895b0978d501c43
48845 a <miror49735@sportrid.com>
2023-12-26 10:16:00 +0000

    add some optimize to new analys JS

--- static/js/analys/new.js
+++ static/js/analys/new.js
@@ -1,103 +1,84 @@
+function toggleButtons(displayCreate, displayAddRow, displaySaveData) {
+  document.getElementById('create-table-btn').style.display = displayCreate ? "none" : "inline-block";
+  document.getElementById('add-row-btn').style.display = displayAddRow ? "inline-block" : "none";
+  document.getElementById('save-data-btn').style.display = displaySaveData ? "inline-block" : "none";
+}
+
+function enableButtons(enableAddRow, enableSaveData) {
+  document.getElementById('add-row-btn').disabled = !enableAddRow;
+  document.getElementById('save-data-btn').disabled = !enableSaveData;
+}
+
 function createTable() {
-  var tableHTML = `
-      <table id="materials-table">
-          <thead>
-              <tr>
-                  <th>نام</th>
-                  <th>نماد</th>
-                  <th>توضیحات</th>
-                  <th>موجودی</th>
-                  <th>قیمت</th>
-                  <th>واحد</th>
-                  <th>عملیات</th>
-              </tr>
-          </thead>
-          <tbody>
-              <tr>
-                  <td><input type="text" class="نام ماده"></td>
-                  <td><input type="text" class="رنگ"></td>
-                  <td><input type="text" class="واحد"></td>
-                  <td><input type="number" class="نرخ" value="0"></td>
-                  <td><input type="number" class="تاریخ نرخ گذاری" value="0"></td>
-                  <td>
-                      <select class="unit">
-                          <option value="Unit1">متر</option>
-                          <option value="Unit2">عدد</option>
-                      </select>
-                  </td>
-                  <td><button onclick="deleteRow(this)">حذف ماده</button></td>
-              </tr>
-          </tbody>
-      </table>
-      <button onclick="addRow()">افزودن ماده اولیه جدید</button>
-      <button onclick="saveData()">ذخیره</button>
+  const tableContainer = document.getElementById('table-container');
+  tableContainer.innerHTML = `
+    <table id="materials-table">
+      <thead>
+        <tr>
+          <th>نام</th>
+          <th>نماد</th>
+          <th>توضیحات</th>
+          <th>موجودی</th>
+          <th>قیمت</th>
+          <th>واحد</th>
+          <th>عملیات</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${createTableRow()}
+      </tbody>
+    </table>
+    <button onclick="addRow()">افزودن ماده اولیه جدید</button>
+    <button onclick="saveData()">ذخیره</button>
   `;
-  document.getElementById('table-container').innerHTML = tableHTML;
-  document.getElementById('create-table-btn').style.display = "none"; // Hide the create button
-  document.getElementById('add-row-btn').style.display = "inline-block"; // Show add row button
-  document.getElementById('save-data-btn').style.display = "inline-block"; // Show save button
-  document.getElementById('add-row-btn').disabled = false; // Enable the add row button
-  document.getElementById('save-data-btn').disabled = false; // Enable the save button
+  toggleButtons(true, true, true);
+  enableButtons(true, true);
+}
+
+function createTableRow() {
+  // Assuming this is the template for new rows which can be created dynamically.
+  return `
+    <tr>
+      <td><input type="text" class="نام ماده"></td>
+      <td><input type="text" class="رنگ"></td>
+      <td><input type="text" class="واحد"></td>
+      <td><input type="number" class="نرخ" value="0"></td>
+      <td><input type="number" class="تاریخ نرخ گذاری" value="0"></td>
+      <td>
+        <select class="unit">
+          <option value="Unit1">متر</option>
+          <option value="Unit2">عدد</option>
+        </select>
+      </td>
+      <td><button onclick="deleteRow(this)">حذف ماده</button></td>
+    </tr>
+  `;
 }
 
 function addRow() {
-    const table = document.getElementById('materials-table').getElementsByTagName('tbody')[0];
-    const newRow = table.insertRow(table.rows.length);
-    const columns = ['نام', 'نماد', 'توضیحات', 'موجودی', 'قیمت', 'واحد', 'action'];
+  const tableBody = document.getElementById('materials-table').querySelector('tbody');
+  tableBody.insertAdjacentHTML('beforeend', createTableRow());
+}
 
-    columns.forEach((col, index) => {
-      const cell = newRow.insertCell(index);
-      if (col === 'unit') {
-        const select = document.createElement('select');
-        select.classList.add('unit');
-        const units = ['متر', 'عدد']; // Replace with your list of units from Django
-        units.forEach(unit => {
-          const option = document.createElement('option');
-          option.value = unit;
-          option.text = unit;
-          select.appendChild(option);
-        });
-        cell.appendChild(select);
-      } else if (col === 'action') {
-        const button = document.createElement('button');
-        button.textContent = 'حذف ماده';
-        button.onclick = function() {
-          deleteRow(this);
-        };
-        cell.appendChild(button);
-      } else {
-        const input = document.createElement('input');
-        input.type = col === 'quantity' || col === 'price' ? 'number' : 'text';
-        input.classList.add(col);
-        input.value = col === 'quantity' || col === 'price' ? 0 : '';
-        cell.appendChild(input);
-      }
-    });
-  }
+function deleteRow(button) {
+  const row = button.closest('tr');
+  row.remove();
+}
 
-  function deleteRow(btn) {
-    const row = btn.parentNode.parentNode;
-    row.parentNode.removeChild(row);
-  }
+function saveData() {
+  const data = Array.from(document.querySelectorAll('#materials-table tbody tr')).map(row => {
+    const entries = row.querySelectorAll('input, select');
+    return Array.from(entries).reduce((obj, entry) => {
+      obj[entry.className] = entry.value;
+      return obj;
+    }, {});
+  });
 
-  function saveData() {
-    const table = document.getElementById('materials-table');
-    const rows = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
-    const data = [];
+  console.log(data);
+  // Add further logic to post this data to the server.
+}
 
-    for (let i = 0; i < rows.length; i++) {
-      const cells = rows[i].getElementsByTagName('td');
-      const rowData = {};
-      for (let j = 0; j < cells.length - 1; j++) {
-        const cell = cells[j].querySelector('input, select');
-        rowData[cell.classList[0]] = cell.value;
-      }
-      data.push(rowData);
-    }
-
-    console.log(data);
-    // Add logic to handle the data (e.g., sending it to a server, etc.)
-  }
+// Fetch Materials and SweetAlert Logic remains mostly unchanged as no immediate optimization is clear without knowing backend logic.
 
   function selectMaterial() {
     // Make a request to the server to get the materials
