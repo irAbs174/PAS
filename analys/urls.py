06e5519c0417d0ea1ad492fe0440a14cac3a64e0
48845 a <miror49735@sportrid.com>
2023-12-27 07:37:52 +0000

    LOVE

--- analys/urls.py
+++ analys/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import (new_analys, materials_list)
+from .views import (new_analys,
+materials_list,
+materials_detail)
 
 urlpatterns = [
     path('new', new_analys, name="start_new_analys"),
     path('materials_list', materials_list),
+    path('material_detail', materials_detail),
 ]
